935737cfb683a1fb87a31fe734b375f050d941d6
Sandlee09 <sandlee09@outlook.com>
2017-09-11 20:23:16 -0300

    9/11/2017

--- javascript/main.js
+++ javascript/main.js
@@ -6,66 +6,21 @@ $(document).ready(function() {
 	//Side Panel JS END
 
 	
-
+  	$('#my-slide').hide();
+  	$('.background-wipe').hide();
 	
+	$('.img_1').click(function() {
+		$('#my-slide').show();
+		$('.background-wipe').show();
+	})
 
+	$('.exit').click(function() {
+		$('#my-slide').hide();
+  		$('.background-wipe').hide();
+	})
 	
-
-	//Centro Tradicionalista Courasel
-	$('.img_1').click(function () {main(5, 'centro')});
-
-	//Pueblo Courasel
-	$('.img_2').click(function () {main(5, 'pueblo')});
-
-	//Desfile Courasel
-	$('.img_3').click(function () {main(5, 'desfile')});
-
-	//Gente Courasel
-	$('.img_4').click(function () {main(5, 'gente')});
-
 
 });
-
-
-//courasel function
-function main(picAmount, folderName) {
-		var array_slot = 1;
-		
-		$('.MainScreen').hide();
-		$('.courasel').css('opacity', 1);
-		$('.courasel').show();
-		$('body').css('background-color', 'black');
-		
-
-		$('.right_arrow').click(function() {
-			if(array_slot < picAmount ) {
-				array_slot++;
-				$('.image_rotation').attr('src', 'images/'+ folderName +'/' + (array_slot) + '.jpg');
-			} else{
-				array_slot = 1;
-				$('.image_rotation').attr('src', 'images/'+ folderName +'/' + (array_slot) + '.jpg')};
-		});
-
-		$('.left_arrow').click(function() {
-			if(array_slot > 1 ) {
-				array_slot--;
-				$('.image_rotation').attr('src', 'images/'+ folderName +'/' + (array_slot) + '.jpg');
-			} else{
-				array_slot = picAmount;
-				$('.image_rotation').attr('src', 'images/'+ folderName +'/' + (array_slot) + '.jpg');}
-		});
-
-		$('.image_rotation').attr('src', 'images/'+ folderName + '/1.jpg');
-		
-
-
-		$('.close_btn').click(function() {
-			$('body').css('background-color', 'white');
-			$('body').css('opacity', 1);
-			$('.courasel').hide();
-			$('.MainScreen').show();
-		});
-};
 
 
 
